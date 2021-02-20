fc36dcadcdbf0c09d137badffc6882ee138dddde
alex <alexb7217@gmail.com>
2021-02-20 08:09:15 -0800

    Smol edits to content

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => {
-  return <div>React App With Webpack</div>;
+  return <div>React App With Webpack - ready for Dokku!</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));

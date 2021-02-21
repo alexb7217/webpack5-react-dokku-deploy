5b11e986511719b26cd5e228871412a29dc8b884
alex <alexb7217@gmail.com>
2021-02-21 11:09:11 -0800

    Introduce basic style demo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import style from './app.css';
 
 const Index = () => {
-  return <div>React App With Webpack - ready for Dokku!</div>;
+  return (
+    <div>
+      <div>
+        <h1>React App With Webpack - ready for Dokku!</h1>
+      </div>
+      <div className={style.app}>
+        <p>This text will be blue.</p>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));

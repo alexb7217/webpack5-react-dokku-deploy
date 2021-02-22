d8d4d11475fba1305fda359d2c3ca5ee952f3f41
alex <alexb7217@gmail.com>
2021-02-22 08:47:59 -0800

    webpack likes bundle again

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'main.js'
+    filename: 'bundele.js'
   },
   module: {
     rules: [

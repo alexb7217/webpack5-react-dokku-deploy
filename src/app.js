35197fe9c57c082c831a5e3dddca7d65036168b1
alex <alexb7217@gmail.com>
2021-02-26 04:58:56 -0800

    Organize index page tests a little

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const Index = () => {
     <div>
       <div className={style.app}>
         <h1>React App With Webpack - ready for Dokku!</h1>
+        <h3>Webpack Tests</h3>
+        <h4>CSS Stylesheets</h4>
         <p>This 'p' text will be blue.</p>
         <span>This 'span' will be green</span>
         <ul>
@@ -21,9 +23,12 @@ const Index = () => {
           <li>Another red list item</li>
           <li>Red list item added for extra listinenss</li>
         </ul>
+        <h4>JS and LoDash</h4>
         <p>{webpack_text}</p>
-        <h3>Profile Image:</h3>
-        <img src={Icon} alt="Icon" /> 
+        <h4>Static Images</h4>
+        <p>Profile Image:</p>
+        <img src={Icon} alt="Icon" />
+        <h4>Static JSON and JSON 5 Data</h4>
         <p>{Data.mydata}</p>
         <ul>
           <li>Name: {json.owner.name}</li>
